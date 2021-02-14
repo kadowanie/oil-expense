ed40fa39201474953eba130e1e6c4bc82ad6afff
Student <student@itpark.tech>
2021-02-14 14:40:05 +0300

    change vrble name

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int oilLevel = 50;
+        int oilVolume = 50;
         double expense = 7.2;
         double distance;
-        distance = 100 * (oilLevel / expense);
+        distance = 100 * (oilVolume / expense);
         System.out.println(distance);
     }
 }
