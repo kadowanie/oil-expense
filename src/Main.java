572bdc0d9f288e099793ea79ef1eec7c26ecf857
Student <student@itpark.tech>
2021-02-14 14:30:10 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int oilLevel = 50;
+        double expense = 7.2;
+        double distance;
+        distance = 100 * (oilLevel / expense);
+        System.out.println(distance);
+    }
+}
